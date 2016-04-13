b56b040123398b6c4ff70ff6748ffbd66e44e6ed
keshav nagpal <keshav.nagpal.1@gmail.com>
2016-04-13 21:13:37 +0530

    Update URLs.py

--- learndjango/msg/URLs.py
+++ learndjango/msg/URLs.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns,url
 from msg import views
 
-urlpatterns = patterns('',url(r'^$',views.index,name="index"))
+urlpatterns = [
+  url('',url(r'^$',views.index,name="index")
+  ]
                        
